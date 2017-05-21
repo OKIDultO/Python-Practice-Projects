9b6d10cb7ef4175d14ae7a7e0ed24400bbea85ca
Ziquan Li <ziquan.li@outlook.com>
2017-05-21 15:03:08 -0700

    Draft2
    题目：输入三个整数x,y,z，请把这三个数由小到大输出。
    程序分析：我们想办法把最小的数放到x上，先将x与y进行比较，如果x>y则将x与y的值进行交换，然后再用x与z进行比较，如果x>z则将x与z的值进行交换，这样能使x最小。
    
    '''
    优化：使用列表方法添加对象及排序
    '''

--- 005/005.py
+++ 005/005.py
@@ -3,23 +3,12 @@
 程序分析：我们想办法把最小的数放到x上，先将x与y进行比较，如果x>y则将x与y的值进行交换，然后再用x与z进行比较，如果x>z则将x与z的值进行交换，这样能使x最小。
 '''
 
-x = int(input("请输入一个整数："))
-y = int(input("请输入一个整数："))
-z = int(input("请输入一个整数："))
+l = []
 
-if x > y:
-	temp = x
-	x = y
-	y = temp
+for i in range(3):
+	x = int(input("请输入一个整数："))
+	l.append(x)
 
-if x > z:
-	temp = x
-	x = z
-	z = temp
-	
-if y > z:
-	temp = y
-	y = z
-	z = temp
+l.sort()
 
-print("三个数由小到大的排序为：", x, y, z)
+print("三个数由小到大的排序为：", l[0], l[1], l[2])
