78880e050c4e9c3c1c9c9d0912c52914d83e667f
Ziquan Li <ziquan.li@outlook.com>
2017-05-23 00:00:55 -0700

    Draft2
    题目：输出 9*9 乘法口诀表。
    程序分析：分行与列考虑，共9行9列，i控制行，j控制列。
    
    '''
    优化输出效果
    '''

--- 008/008.py
+++ 008/008.py
@@ -6,5 +6,6 @@
 print("9*9 乘法口诀表：")
 
 for i in range(1, 10):
-	for j in range(1, 10):
-		print("%d * %d = %d" %(i, j, i*j))
+	print()
+	for j in range(1, i+1):
+		print("%d * %d = %d\t" %(i, j, i*j), end="")
