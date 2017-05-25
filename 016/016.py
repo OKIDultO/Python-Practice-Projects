913a3e2aea3f78242d9538bc9a97db5500fbc021
Ziquan Li <ziquan.li@outlook.com>
2017-05-25 15:22:29 -0700

    Draft2
    题目：输出日期。
    程序分析：使用 datetime 模块。

--- 016/016.py
+++ 016/016.py
@@ -1,8 +1,24 @@
 '''
-题目：输出今天日期。
+题目：输出日期。
 程序分析：使用 datetime 模块。
 '''
 
 import datetime
 
-print(datetime.date.today().strftime('%m/%d/%Y'))
+# 输出今日日期，格式为 dd/mm/yyyy。更多选项可以查看 strftime() 方法
+print(datetime.date.today().strftime('%d/%m/%Y'))
+ 
+# 创建日期对象
+miyazakiBirthDate = datetime.date(1941, 1, 5)
+ 
+print(miyazakiBirthDate.strftime('%d/%m/%Y'))
+ 
+# 日期算术运算
+miyazakiBirthNextDay = miyazakiBirthDate + datetime.timedelta(days=1)
+ 
+print(miyazakiBirthNextDay.strftime('%d/%m/%Y'))
+ 
+# 日期替换
+miyazakiFirstBirthday = miyazakiBirthDate.replace(year=miyazakiBirthDate.year + 1)
+ 
+print(miyazakiFirstBirthday.strftime('%d/%m/%Y'))
