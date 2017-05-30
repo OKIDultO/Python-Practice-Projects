f3e46a7462374131109c7c0b4c0e2aa86b9a744a
Ziquan Li <ziquan.li@outlook.com>
2017-05-30 10:51:00 -0700

    Draft2
    题目：求1+2!+3!+...+20!的和。
    程序分析：此程序只是把累加变成了累乘。
    
    '''
    语句优化
    '''

--- 025/025.py
+++ 025/025.py
@@ -3,14 +3,11 @@
 程序分析：此程序只是把累加变成了累乘。
 '''
 
-def Pro_Ope(x):
-	product = 1
-	for i in range(2, x+1):
-		product *= i
-	return product
-	
+product = 1
 sum = 0
+
 for i in range(1, 21):
-	sum += Pro_Ope(i)
+	product *= i
+	sum += product
 	
 print("1+2!+3!+...+20!的和为%d" %sum)
