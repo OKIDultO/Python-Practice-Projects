c463b797648b95ea908720ea0c88a61af795545a
Ziquan Li <ziquan.li@outlook.com>
2017-05-31 13:47:52 -0700

    Draft1
    题目：利用递归函数调用方式，将所输入的5个字符，以相反顺序打印出来。
    程序分析：无。

--- 027/027.py
+++ 027/027.py
@@ -2,3 +2,15 @@
 题目：利用递归函数调用方式，将所输入的5个字符，以相反顺序打印出来。
 程序分析：无。
 '''
+
+def List(i):
+	if i == 0:
+		return list
+	else:
+		x = (input(""))
+		list.insert(0, x)
+		return List(i-1)
+
+list = []
+print("请输入5个字符：")
+print("反序输出结果为：\n%s" %List(5))
