73efeeef06a40c830b5329769814545f8b0da147
Ziquan Li <ziquan.li@outlook.com>
2017-06-01 15:20:29 -0700

    Draft2
    题目：请输入星期几的第一个字母来判断一下是星期几，如果第一个字母一样，则继续判断第二个字母。
    程序分析：用情况语句比较好，如果第一个字母一样，则判断用情况语句或if语句判断第二个字母。
    
    '''
    优化判断语句
    '''

--- 031/031.py
+++ 031/031.py
@@ -3,7 +3,7 @@
 程序分析：用情况语句比较好，如果第一个字母一样，则判断用情况语句或if语句判断第二个字母。
 '''
 
-x = input("判断星期几\n请输入第一个字母:")
+x = input("判断星期几（首字母大写）\n请输入第一个字母:")
 
 if x == "M":
 	print("星期一")
@@ -11,15 +11,21 @@ elif x == "T":
 	y = input("请输入第二个字母:")
 	if y == "u":
 		print("星期二")
-	else:
+	elif y == "h":
 		print("星期四")
+	else:
+		print("输入有误！")
 elif x == "W":
 	print("星期三")
 elif x == "F":
 	print("星期五")
-else:
+elif x == "S":
 	y = input("请输入第二个字母:")
 	if y == "a":
 		print("星期六")
-	else:
+	elif y == "u":
 		print("星期日")
+	else:
+		print("输入有误！")
+else:
+	print("输入有误！")
