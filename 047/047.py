bb1247a4ddd9f708b195b645df5394779e7a9f1e
Ziquan Li <ziquan.li@outlook.com>
2017-06-05 14:49:51 -0700

    Draft1
    题目：两个变量值互换。
    程序分析：无

--- 047/047.py
+++ 047/047.py
@@ -2,3 +2,14 @@
 题目：两个变量值互换。
 程序分析：无
 '''
+
+def exchange(a,b):
+    a,b = b,a
+    return (a,b)
+ 
+if __name__ == '__main__':
+    x = 10
+    y = 20
+    print('x = %d,y = %d' % (x,y))
+    x,y = exchange(x,y)
+    print('x = %d,y = %d' % (x,y))
