509f623af3d55f2b0a2b1214560f9d5f5ebd4388
Ziquan Li <ziquan.li@outlook.com>
2017-06-05 15:18:03 -0700

    Draft1
    题目：数字比较。
    程序分析：无

--- 048/048.py
+++ 048/048.py
@@ -2,3 +2,12 @@
 题目：数字比较。
 程序分析：无
 '''
+
+compare = []
+
+x = compare.append(int(input("请输入一个数：")))
+y = compare.append(int(input("再输入一个数：")))
+
+compare.sort()
+
+print("较大的数为：%d\n较小的数为：%d" %(compare[1], compare[0]))
